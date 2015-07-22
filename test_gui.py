9d9e69c1a9dd3206ae7150fd83982ce365c00a85
Morgan Fine-Morris <mfinemorris@icloud.com>
2015-07-22 13:05:39 -0700

    commented out separators section for now. adjusted some comments in initUI and made moved openicon to the __init__ function (and made it an instance variable). Also added body of load_file_preview. It is still untested.

--- test_gui.py
+++ test_gui.py
@@ -1,41 +1,21 @@
 #creating a tabbed interface:
 #http://ubuntuforums.org/showthread.php?t=1144712 
-import sys
-import inspect
-import pandas
+import sys, os.path, inspect
+import pandas as pd
 
-from PyQt4 import QtGui as qtGui
-from PyQt4 import QtCore as qtCore
-from PyQt4.QtCore import pyqtSlot
-
-#undoicon = QIcon.fromTheme("edit-undo")
-#self.style().standardIcon(QtGui.QStyle.SP_DialogOpenButton)
-import os.path
-#os.path.expanduser("~") #to get the root
-#filename = QFileDialog.getOpenFileName(w, 'Open File', os.path.expanduser("~"))
-
-def 
-"""
-http://stackoverflow.com/questions/10636024/python-pandas-gui-for-viewing-a-dataframe-or-matrix
-answered Aug 20 '12 at 11:30 by user1319128
-"""
-
-df  = read_csv(filename, index_col = 0,header = 0)
-self.datatable = QtGui.QTableWidget(parent=self)
-self.datatable.setColumnCount(len(df.columns))
-self.datatable.setRowCount(len(df.index))
-for i in range(len(df.index)):
-    for j in range(len(df.columns)):
-        self.datatable.setItem(i, j, QtGui.QTableWidgetItem(str(df.iget_value(i, j))))
+from PyQt4 import QtGui as qtg
+from PyQt4 import QtCore as qtc
+from PyQt4.QtCore import pyqtSlot as qtslot
 
 
-
-class Example(qtGui.QWidget):
+class Example(qtg.QWidget):
 
     def __init__(self, width=300, height=300):
         super(Example, self).__init__()
         
         self.curr_dir = os.path.expanduser("~")
+        self.openicon = qtg.QIcon("pics/open_folder.png") #'file dialog' icon
+
         
         self.initUI(width, height)
         
@@ -46,13 +26,13 @@ class Example(qtGui.QWidget):
             TODO: error catching. maybe set a current_directory flag here?
         """
         
-        filename  = qtGui.QFileDialog.getOpenFileName(self, "Choose Data File", self.curr_dir)
+        filepath  = qtg.QFileDialog.getOpenfilepath(self, "Choose Data File", self.curr_dir)
             
-        if filename:
-            self.filename = filename
-            self.input_datapath.setText(self.filename)
+        if filepath:
+            self.filepath = filepath
+            self.input_datapath.setText(self.filepath)
             try:
-                self.curr_dir = os.path.dirname(str(self.filename))
+                self.curr_dir = os.path.dirname(str(self.filepath))
                 self.output_datapath.setText(self.curr_dir)
             except Exception as e:
                 print e
@@ -62,37 +42,51 @@ class Example(qtGui.QWidget):
         """Get the name of the folder where files output by the program
         should be saved."""
             
-        directory = qtGui.QFileDialog.getExistingDirectory(self, "Choose Output Directory", self.curr_dir)
+        directory = qtg.QFileDialog.getExistingDirectory(self, "Choose Output Directory", self.curr_dir)
         if directory:
             self.output_directory = directory
             self.curr_dir = directory
             self.output_datapath.setText(self.output_directory)
         
-    def load_file_preview(self):
-        pass
-        
     def set_sep(self):
         print self.btn_group.checkedButton()
     
+    def load_file_preview(self):
+        """
+        modified from answer to stackoverflow question 10636024
+        answer provided by user1319128 at Aug 20 '12 at 11:30 
+        edited by Rostyslav Dzinko at Aug 20 '12 at 13:28 
+        """
+        if not self.filepath:
+            return
         
-    
-    
+        df  = pd.read_table(self.filepath, index_col = 0,header = 0, nrows=10)
+        print df
+        
+        self.datatable = qtg.QTableWidget(parent=self)
+        self.datatable.setColumnCount(len(df.columns)) #get all columns
+        self.datatable.setRowCount(10)#len(df.index))   #get only the first 10 rows
+        
+        for i in range(len(df.index)):
+            for j in range(len(df.columns)):
+                self.datatable.setItem(i, j, qtg.QTableWidgetItem(str(df.iget_value(i, j))))
+
     def initUI(self, width, height):
         #setup and show window
         self.setGeometry(0, 0, width, height)
         self.setMinimumSize(500,650)
         self.center()
         self.setWindowTitle("Signal Analysis") #once a file has been imported, change the name
-        #self.setWindowIcon(qtGui.QIcon("pics/signal2_temp.png"))#"pics/thicklines.png"))
+        #self.setWindowIcon(qtg.QIcon("pics/signal2_temp.png"))#"pics/thicklines.png"))
     
         """
-        exitAction = qtGui.QAction(qtGui.QIcon("exit.png"), "&Exit", self)
+        exitAction = qtg.QAction(qtg.QIcon("exit.png"), "&Exit", self)
         exitAction.setShortcut("Ctrl+Q")
         exitAction.setStatusTip("Exit Application")
-        exitAction.triggered.connect(qtGui.qApp.quit)
+        exitAction.triggered.connect(qtg.qApp.quit)
         
         #menu bar
-        menubar = qtGui.QMenuBar()
+        menubar = qtg.QMenuBar()
         try:
             import platform
             platform.mac_ver() #should fail if not a mac (test this!)
@@ -103,13 +97,12 @@ class Example(qtGui.QWidget):
         file_menu.addAction(exitAction)
         """
         
-        
         #make tabs_widg widget and tabs
-        tabs_widg = qtGui.QTabWidget(self)
-        load_tab = qtGui.QWidget()
-        eventdet_tab = qtGui.QWidget()
-        postdet_tab = qtGui.QWidget()
-        analysis_tab = qtGui.QWidget()
+        tabs_widg = qtg.QTabWidget(self)
+        load_tab = qtg.QWidget()
+        eventdet_tab = qtg.QWidget()
+        postdet_tab = qtg.QWidget()
+        analysis_tab = qtg.QWidget()
         
         # Add tabs to tabs_widg
         tabs_widg.addTab(load_tab,"&Load Data")
@@ -118,32 +111,32 @@ class Example(qtGui.QWidget):
         tabs_widg.addTab(analysis_tab,"&Interval Analysis")
         
         
-        ###################
+        ########################
         #### LOAD CSVs PAGE ####
-        load_page = qtGui.QVBoxLayout(load_tab) # connects load page and tab
+        ########################
+
+        load_page = qtg.QVBoxLayout(load_tab) # connects load page and tab
         
         ### Page Components
 
-        openicon = qtGui.QIcon("pics/open_folder.png") #open file dialog icon
-
-        #input file
-        input_label = qtGui.QLabel('Data File:')
-        self.input_datapath = qtGui.QLineEdit()
-        file_dialog_btn = qtGui.QPushButton(icon=openicon,text="")
-        file_dialog_btn.setIconSize(qtCore.QSize(20, 20))
+        ## make and add input file fields
+        input_label = qtg.QLabel('Data File:')
+        self.input_datapath = qtg.QLineEdit()
+        file_dialog_btn = qtg.QPushButton(icon=self.openicon,text="")
+        file_dialog_btn.setIconSize(qtc.QSize(20, 20))
         
-        inputfile_box = qtGui.QHBoxLayout()
+        inputfile_box = qtg.QHBoxLayout()
         inputfile_box.addWidget(input_label)
         inputfile_box.addWidget(self.input_datapath)
         inputfile_box.addWidget(file_dialog_btn)
         
-        #output folder
-        output_label = qtGui.QLabel('Output Folder:')
-        self.output_datapath = qtGui.QLineEdit()
-        folder_dialog_btn = qtGui.QPushButton(icon=openicon,text="")
-        folder_dialog_btn.setIconSize(qtCore.QSize(20, 20))
+        ## make and add output folder fields
+        output_label = qtg.QLabel('Output Folder:')
+        self.output_datapath = qtg.QLineEdit()
+        folder_dialog_btn = qtg.QPushButton(icon=self.openicon,text="")
+        folder_dialog_btn.setIconSize(qtc.QSize(20, 20))
 
-        outputfolder_box = qtGui.QHBoxLayout()
+        outputfolder_box = qtg.QHBoxLayout()
         outputfolder_box.addWidget(output_label)
         outputfolder_box.addWidget(self.output_datapath)
         outputfolder_box.addWidget(folder_dialog_btn)
@@ -152,7 +145,7 @@ class Example(qtGui.QWidget):
         file_dialog_btn.clicked.connect(self.open_file_dialog)
         folder_dialog_btn.clicked.connect(self.open_dir_dialog)
         
-        filepath_boxes = qtGui.QVBoxLayout()
+        filepath_boxes = qtg.QVBoxLayout()
         filepath_boxes.addLayout(inputfile_box)
         filepath_boxes.addLayout(outputfolder_box)
         
@@ -167,16 +160,17 @@ class Example(qtGui.QWidget):
         load_page.addLayout(filepath_boxes)
         
         # separators
-        separators_label = qtGui.QLabel('Separators')
-        custom_sep_label = qtGui.QLabel('custom')
+        """not dealing with separators yet. It's too much
+        separators_label = qtg.QLabel('Separators')
+        custom_sep_label = qtg.QLabel('custom')
 
-        comma_sep = qtGui.QCheckBox('commas', self)
-        tab_sep = qtGui.QCheckBox('tab', self)
-        space_sep = qtGui.QCheckBox('space', self)
-        custom_sep = qtGui.QCheckBox('custom', self)
-        custom_text = qtGui.QLineEdit('custom', self)
+        comma_sep = qtg.QCheckBox('commas', self)
+        tab_sep = qtg.QCheckBox('tab', self)
+        space_sep = qtg.QCheckBox('space', self)
+        custom_sep = qtg.QCheckBox('custom', self)
+        custom_text = qtg.QLineEdit('custom', self)
         custom_text.setReadOnly(True) #if custom_sep check, change to false
-        self.btn_group = qtGui.QButtonGroup()
+        self.btn_group = qtg.QButtonGroup()
         self.btn_group.addButton(comma_sep)
         self.btn_group.addButton(tab_sep)
         self.btn_group.addButton(space_sep)
@@ -186,7 +180,7 @@ class Example(qtGui.QWidget):
 
         #btn_group.stateChanged.connect(
         
-        separators = qtGui.QHBoxLayout()
+        separators = qtg.QHBoxLayout()
         separators.addWidget(separators_label)
         separators.addWidget(comma_sep)
         separators.addWidget(tab_sep)
@@ -195,24 +189,22 @@ class Example(qtGui.QWidget):
         separators.addWidget(custom_text)
         
         load_page.addLayout(separators)
-
+        """
         
         # row boundaries, time column
-        first_row_label = qtGui.QLabel('First Row (include column labels)')
-        last_row_label = qtGui.QLabel('Last Row')
-        first_row = qtGui.QSpinBox()
-        last_row = qtGui.QSpinBox()
+        first_row_label = qtg.QLabel('First Row (include column labels)')
+        last_row_label = qtg.QLabel('Last Row')
+        first_row = qtg.QSpinBox()
+        last_row = qtg.QSpinBox()
         first_row.setWrapping(True)
         last_row.setWrapping(True)
         
-        time_col_label = qtGui.QLabel('Time Col')
-        time_col = qtGui.QSpinBox()
+        time_col_label = qtg.QLabel('Time Col')
+        time_col = qtg.QSpinBox()
         time_col.setWrapping(True)
         
-        reset_cols_btn = qtGui.QPushButton("&Reset Column Selection")
-
-        
-        row_boundries = qtGui.QHBoxLayout()
+        reset_cols_btn = qtg.QPushButton("&Reset Column Selection")
+        row_boundries = qtg.QHBoxLayout()
         row_boundries.addWidget(first_row_label)
         row_boundries.addWidget(first_row)
         row_boundries.addWidget(last_row_label)
@@ -227,14 +219,11 @@ class Example(qtGui.QWidget):
 
         ### file preview
         
-        file_prev_label = qtGui.QLabel('File Preview')
+        file_prev_label = qtg.QLabel('File Preview')
         
-        model = pandasqt.DataFrameModel()
-        table = qtGui.QTableView()
-        widget.setModel(model)
-        model.setDataFrame(data)
+        table = qtg.QTableView()
         
-        table_box = qtGui.QVBoxLayout()
+        table_box = qtg.QVBoxLayout()
         table_box.addWidget(file_prev_label)
         table_box.addWidget(table)
         load_page.addLayout(table_box)
@@ -245,13 +234,13 @@ class Example(qtGui.QWidget):
         
         
         #set layout
-        vbox = qtGui.QVBoxLayout()
+        vbox = qtg.QVBoxLayout()
         #vbox.addWidget(menubar)
         vbox.addWidget(tabs_widg)
         self.setLayout(vbox)
         
     def center(self):
-        screen = qtGui.QDesktopWidget().screenGeometry()
+        screen = qtg.QDesktopWidget().screenGeometry()
         size = self.geometry()
         self.move((screen.width()-size.width())/2, (screen.height()-size.height())/2) 
         
@@ -259,10 +248,10 @@ class Example(qtGui.QWidget):
 
 def main():
     
-    app = qtGui.QApplication(sys.argv)
+    app = qtg.QApplication(sys.argv)
     #app.setStyle("plastique")
     
-    #d = qtGui.QFileDialog()
+    #d = qtg.QFileDialog()
     #d.show()
     
     ex = Example(800,800) 
